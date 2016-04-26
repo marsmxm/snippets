f492e66d99dccea36c2e5957f2e06fa5dfd15deb
Mu Xian Ming <muxianming@anttree.net>
2016-04-26 20:30:07 +0800

    ex2.42

--- SICP.rkt
+++ SICP.rkt
@@ -312,3 +312,28 @@
                      (enumerate-interval 1 (sub1 j))))
               (enumerate-interval 1 (sub1 i))))
            (enumerate-interval 1 n))))
+
+;; Exercise 2.42
+(define empty-board '())
+
+(define (adjoin-position new-row k rest-of-queens)
+  (cons (cons k new-row) rest-of-queens))
+
+(define (safe? k positions)
+  ...)
+
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position new-row
+                                    k
+                                    rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
