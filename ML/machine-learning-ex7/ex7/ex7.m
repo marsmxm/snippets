920d08ced5e272fe40903afd0901a9d02666182d
Mu Xian Ming <marsmxm@gmail.com>
2017-03-31 00:32:43 +0800

    solutions of ex8

--- ML/machine-learning-ex7/ex7/ex7.m
+++ ML/machine-learning-ex7/ex7/ex7.m
@@ -106,7 +106,7 @@ pause;
 fprintf('\nRunning K-Means clustering on pixels from an image.\n\n');
 
 %  Load an image of a bird
-A = double(imread('bird_small.png'));
+A = double(imread('1.png'));
 
 % If imread does not work for you, you can try instead
 %   load ('bird_small.mat');
@@ -123,7 +123,7 @@ X = reshape(A, img_size(1) * img_size(2), 3);
 
 % Run your K-Means algorithm on this data
 % You should try different values of K and max_iters here
-K = 16; 
+K = 24; 
 max_iters = 10;
 
 % When using K-Means, it is important the initialize the centroids
