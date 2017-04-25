b2d6ec01a3ef1108ee4dfd0bd562cae8b6e2cfa1
Mu Xian Ming <marsmxm@gmail.com>
2017-04-25 21:45:29 +0800

    solution to ex8

--- ML/ex8-reco-and-abnormal/ex8/cofiCostFunc.m
+++ ML/ex8-reco-and-abnormal/ex8/cofiCostFunc.m
@@ -40,10 +40,15 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+for i = 1:num_movies
+    for j = 1:num_users
+        if R(i, j) == 1
+            J += (X(i, :) * Theta(j, :)' - Y(i, j)) ^ 2;
+        end
+    end
+end
 
-
-
-
+J = J / 2;
 
 
 
