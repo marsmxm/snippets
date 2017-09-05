5150c91653910770c6920856b8eaedf103eb43ad
Mu Xian Ming <marsmxm@gmail.com>
2017-09-05 11:14:47 +0800

    Update OneDimAveragingPhaser.java

--- pcdp/miniproject_4/src/main/java/edu/coursera/parallel/OneDimAveragingPhaser.java
+++ pcdp/miniproject_4/src/main/java/edu/coursera/parallel/OneDimAveragingPhaser.java
@@ -108,6 +108,48 @@ public final class OneDimAveragingPhaser {
     public static void runParallelFuzzyBarrier(final int iterations,
             final double[] myNew, final double[] myVal, final int n,
             final int tasks) {
-        runParallelBarrier(iterations, myNew, myVal, n, tasks);
+        Phaser ph = new Phaser(0);
+        ph.bulkRegister(tasks);
+
+        Thread[] threads = new Thread[tasks];
+
+        for (int ii = 0; ii < tasks; ii++) {
+            final int i = ii;
+
+            threads[ii] = new Thread(() -> {
+                double[] threadPrivateMyVal = myVal;
+                double[] threadPrivateMyNew = myNew;
+
+                for (int iter = 0; iter < iterations; iter++) {
+                    final int left = i * (n / tasks) + 1;
+                    threadPrivateMyNew[left] = (threadPrivateMyVal[left - 1] + threadPrivateMyVal[left + 1]) / 2.0;
+                    final int right = (i + 1) * (n / tasks);
+                    threadPrivateMyNew[right] = (threadPrivateMyVal[right - 1] + threadPrivateMyVal[right + 1]) / 2.0;
+
+                    int phase = ph.arrive();
+
+                    for (int j = left + 1; j <= right - 1; j++) {
+                        threadPrivateMyNew[j] = (threadPrivateMyVal[j - 1]
+                                + threadPrivateMyVal[j + 1]) / 2.0;
+                    }
+
+                    ph.awaitAdvance(phase);
+
+                    double[] temp = threadPrivateMyNew;
+                    threadPrivateMyNew = threadPrivateMyVal;
+                    threadPrivateMyVal = temp;
+                }
+            });
+            threads[ii].start();
+        }
+
+        for (int ii = 0; ii < tasks; ii++) {
+            try {
+                threads[ii].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     }
 }
