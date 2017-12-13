1e3121f1b636b026e7b7b28e9cd5bfe9c8313ed1
Mu Xian Ming <marsmxm@gmail.com>
2017-12-13 16:18:09 +0800

    Update WeightedUserProfileBuilder.java

--- Recommenders/Assignment02/cbf-assignment/src/main/java/org/lenskit/mooc/cbf/WeightedUserProfileBuilder.java
+++ Recommenders/Assignment02/cbf-assignment/src/main/java/org/lenskit/mooc/cbf/WeightedUserProfileBuilder.java
@@ -29,6 +29,24 @@ public class WeightedUserProfileBuilder implements UserProfileBuilder {
 
         // TODO Normalize the user's ratings
         // TODO Build the user's weighted profile
+        Double rateSum = 0.0;
+        for (Rating r : ratings) {
+            rateSum += r.getValue();
+        }
+        Double rateAvg = rateSum / ratings.size();
+        
+        for (Rating r: ratings) {
+            Map<String, Double> itemVector = model.getItemVector(r.getItemId());
+            for (Map.Entry<String, Double> entry : itemVector.entrySet()) {
+                String tag = entry.getKey();
+                Double value = (r.getValue() - rateAvg) * entry.getValue();
+                if (profile.containsKey(tag)) {
+                    profile.put(tag, profile.get(tag) + value);
+                } else {
+                    profile.put(tag, value);
+                }
+            }
+        }
 
 
         // The profile is accumulated, return it.
