fa933a321eafc9863c450ad4f680590fbd1ff525
Mu Xian Ming (Sam) <mu.xianming@linecorp.com>
2018-06-04 10:55:39 +0800

    j9mm

--- tutorials/flow/src/test/java/host/mxm/flow/J9mm.java
+++ tutorials/flow/src/test/java/host/mxm/flow/J9mm.java
@@ -2,9 +2,6 @@ package host.mxm.flow;
 
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.VarHandle;
-import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
 
 import org.junit.Test;
@@ -17,28 +14,21 @@ public class J9mm {
 
     @Test
     public void plain() throws InterruptedException {
-        Point point = new Point();
-        int count = 100;
-        long factor = 1000000000000000L;
+        Shared shared = new Shared();
+        System.out.println(shared.value);
+        int count = 2;
         var startLatch = new CountDownLatch(1);
         var stopLatch = new CountDownLatch(count);
 
         for (int i = 0; i < count; i++) {
             if (i % 2 == 0) {
-                int finalI = i;
                 new Thread(() -> {
                     try {
                         startLatch.await();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    if (finalI % 4 == 0) {
-                        point.writePlain(
-                                BigInteger.valueOf(Long.MAX_VALUE ).multiply(BigInteger.TWO),
-                                BigInteger.valueOf(Long.MAX_VALUE ).multiply(BigInteger.TWO));
-                    } else {
-                        point.writePlain(BigInteger.ZERO, BigInteger.ZERO);
-                    }
+                    shared.writePlain(Long.MAX_VALUE);
                     stopLatch.countDown();
                 }).start();
             } else {
@@ -48,10 +38,7 @@ public class J9mm {
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    BigInteger[] axises = point.readPlain();
-                    if (!Objects.equals(axises[0], axises[1])) {
-                        System.out.println(Arrays.toString(axises));
-                    }
+                    while (shared.readPlain() == 0) {}
                     stopLatch.countDown();
                 }).start();
             }
@@ -62,27 +49,26 @@ public class J9mm {
     }
 }
 
-class Point {
-    BigInteger x, y;
-    private static final VarHandle X;
-    private static final VarHandle Y;
+class Shared {
+    long value;
+    private static final VarHandle VALUE;
 
     static {
         try {
-            X = MethodHandles.lookup().findVarHandle(Point.class, "x", BigInteger.class);
-            Y = MethodHandles.lookup().findVarHandle(Point.class, "y", BigInteger.class);
+            VALUE = MethodHandles.lookup().findVarHandle(Shared.class, "value", long.class);
         } catch (ReflectiveOperationException e) {
             throw new Error(e);
         }
     }
 
-    public void writePlain(BigInteger x, BigInteger y) {
-        X.set(this, x);
-        Y.set(this, y);
+    public void writePlain(long value) {
+//        VALUE.set(this, value);
+        this.value = value;
     }
 
-    public BigInteger[] readPlain() {
-        return new BigInteger[] { (BigInteger) X.get(this), (BigInteger) Y.get(this) };
+    public long readPlain() {
+//        return (long) VALUE.get(this);
+        return value;
     }
 
 }
