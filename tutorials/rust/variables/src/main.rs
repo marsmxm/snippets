6df4d40fc60216dad91ceb099d14200a0fe72e5a
Mu Xian Ming (Sam) <mu.xianming@linecorp.com>
2018-08-13 15:56:49 +0800

    update

---
+++ tutorials/rust/variables/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+
+    let mut s = String::from("hello");
+    s.push_str(", world!"); // push_str() appends a literal to a String
+    println!("{}", s); // This will print `hello, world!`
+
+    let s1 = String::from("hello");
+    let s2 = s1;
+    println!("{}, world!", s2);
+
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+    println!("s1 = {}, s2 = {}", s1, s2);
+}
