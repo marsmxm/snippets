e8f0522591230ae2bd83f50c46e9b033381252a4
Mu Xian Ming (Sam) <mu.xianming@linecorp.com>
2018-08-16 22:11:43 +0800

    rust update

---
+++ tutorials/rust/enums/src/main.rs
@@ -0,0 +1,74 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+enum IpAddrNew {
+    V4(String),
+    V6(String),
+}
+
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    let home = IpAddrNew::V4(String::from("127.0.0.1"));
+    let loopback = IpAddrNew::V6(String::from("::1"));
+
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("{:?}", six);
+    println!("{:?}", none);
+
+    let some_u8_value = Some(0u8);
+    if let Some(x) = some_u8_value {
+        println!("x={}", x);
+    }
+}
