516f4ea95dd1bd7e7f11cc7c76a99cfc08ae0d27
Mu Xian Ming (Sam) <mu.xianming@linecorp.com>
2018-11-14 16:29:38 +0800

    update subtyping.rkt

--- subtyping.rkt
+++ subtyping.rkt
@@ -1,8 +1,8 @@
 #lang typed/racket
 
-(struct pt ([x : Real] [y : Real]))
-(struct color-pt pt ([c : String]))
-(struct color-3d-pt pt ([z : Real]))
+(struct pt ([x : Real] [y : Real]) #:transparent)
+(struct color-pt pt ([c : String]) #:transparent)
+(struct color-3d-pt pt ([z : Real]) #:transparent)
 
 (struct circle ([center : pt] [r : Real]))
 (struct color-circle ([center : color-pt] [r : Real]))
@@ -19,22 +19,35 @@
          0
          pt-list))
 
+(: replace-first (-> (MListof pt) Real Real Void))
+(define (replace-first pts x y)
+  (when (not (null? pts))
+      (set-mcar! pts (pt x y))))
+
 (: area (-> circle Real))
 (define (area c)
   (* pi (sqr (circle-r c))))
-    
+
+(: transform-then-dist (-> (-> color-pt pt) color-pt Real))
+(define (transform-then-dist f cpt)
+  (dist-to-origin (f cpt)))
 
 (: f1 (-> color-pt pt))
 (define (f1 cp)
   (pt (pt-x cp) (pt-y cp)))
 
-(: f2 (-> pt color-pt))
-(define (f2 p)
+(: f2 (-> color-pt color-pt))
+(define (f2 cpt)
+  cpt)
+
+(: f3 (-> pt color-pt))
+(define (f3 p)
   (color-pt (add1 (pt-x p)) (sub1 (pt-y p)) "black"))
 
-(: f3 (-> color-3d-pt pt))
-(define (f3 c3p)
+(: f4 (-> color-3d-pt pt))
+(define (f4 c3p)
   (pt (pt-x c3p) (color-3d-pt-z c3p)))
+
 
 (: tf (-> (-> color-pt pt) color-pt pt))
 (define (tf f cp)
@@ -48,6 +61,10 @@
 
 (: pts (Listof pt))
 (define pts color-pts)
+
+(: m-pts (MListof pt))
+(define m-pts
+  (mcons (pt 1 1) '()))
 
 (: m-color-pts (MListof color-pt))
 (define m-color-pts
