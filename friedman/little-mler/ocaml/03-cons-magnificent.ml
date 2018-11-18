13e83bec672a1890c97cafb160d13628b7db908f
Mu Xian Ming <marsmxm@gmail.com>
2018-11-18 23:13:49 +0800

    update little mler

---
+++ friedman/little-mler/ocaml/03-cons-magnificent.ml
@@ -0,0 +1,20 @@
+type pizza =
+  Crust
+| Cheese of pizza
+| Onion of pizza
+| Anchovy of pizza
+| Sausage of pizza
+
+let rec remove_anchovy = function
+  | Crust -> Crust
+  | Cheese x -> Cheese (remove_anchovy x)
+  | Onion x -> Onion (remove_anchovy x)
+  | Anchovy x -> remove_anchovy x
+  | Sausage x -> Sausage (remove_anchovy x)
+
+let rec top_anchovy_with_cheese = function
+  | Crust -> Crust
+  | Cheese x -> Cheese (top_anchovy_with_cheese x)
+  | Onion x -> Onion (top_anchovy_with_cheese x)
+  | Anchovy x -> Cheese (Anchovy (top_anchovy_with_cheese x))
+  | Sausage x -> Sausage (top_anchovy_with_cheese x)
