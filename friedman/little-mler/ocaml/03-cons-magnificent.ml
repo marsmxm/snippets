60098794d2ec2941ccf218f76cc01f9bbd364186
Mu Xian Ming (Sam) <mu.xianming@linecorp.com>
2018-11-20 16:37:42 +0800

    chapter 4

--- friedman/little-mler/ocaml/03-cons-magnificent.ml
+++ friedman/little-mler/ocaml/03-cons-magnificent.ml
@@ -18,3 +18,10 @@ let rec top_anchovy_with_cheese = function
   | Onion x -> Onion (top_anchovy_with_cheese x)
   | Anchovy x -> Cheese (Anchovy (top_anchovy_with_cheese x))
   | Sausage x -> Sausage (top_anchovy_with_cheese x)
+
+let rec subst_anchovy_with_cheese = function
+  | Crust -> Crust
+  | Cheese x -> Cheese (subst_anchovy_with_cheese x)
+  | Onion x -> Onion (subst_anchovy_with_cheese x)
+  | Anchovy x -> Cheese (subst_anchovy_with_cheese x)
+  | Sausage x -> Sausage (subst_anchovy_with_cheese x)
