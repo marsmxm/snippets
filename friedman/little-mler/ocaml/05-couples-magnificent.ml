7624b03cb180ab081b8c24c93934e45953512a31
Mu Xian Ming (Sam) <mu.xianming@linecorp.com>
2018-11-26 11:12:55 +0800

    update

--- friedman/little-mler/ocaml/05-couples-magnificent.ml
+++ friedman/little-mler/ocaml/05-couples-magnificent.ml
@@ -32,3 +32,28 @@ let rec rem_fish = function
     then rem_fish (x, p)
     else Topping (t, rem_fish (x, p))
 
+let eq_int : int * int -> bool = fun (m, n) -> m = n
+
+let rec rem_int = function
+  | (x, Bottom) -> Bottom
+  | (x, Topping (t, p)) ->
+    if eq_int (x, t) then rem_int (x, p) else Topping (t, rem_int (x, p))
+
+let rec rem = fun (f, p) ->
+  match p with
+  | Bottom -> Bottom
+  | Topping (t, p') ->
+    if f t then rem (f, p') else Topping (t, rem (f, p'))
+
+let rem_fish2 (x, p) = rem ((fun x' -> eq_fish (x, x')), p)
+let rem_int2 (x, p) = rem ((fun x' -> eq_int (x, x')), p)
+
+
+type num =
+    Zero
+  | One_more_than of num
+
+let rec eq_num = function
+  | (Zero, Zero) -> true
+  | (One_more_than n1, One_more_than n2) -> eq_num (n1, n2)
+  | _ -> false
