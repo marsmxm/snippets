7ff1efb19a88765bf35bd5d0b1b953c7b1957916
Mu Xian Ming (Sam) <mu.xianming@linecorp.com>
2019-06-17 12:10:37 +0800

    udpate lambda.rkt

--- redex/lambda.rkt
+++ redex/lambda.rkt
@@ -73,3 +73,54 @@
             (λ-add m ((t t) (λ-sub1 n) m))))))
 (define mult
   (mk-mult mk-mult))
+
+(((λ (mksum)
+    (λ (n m)
+      (if (zero? n)
+          m
+          (add1 ((mksum mksum) (sub1 n) m)))))
+  (λ (mksum)
+    (λ (n m)
+      (if (zero? n)
+          m
+          (add1 ((mksum mksum) (sub1 n) m))))))
+ 2 3)
+
+(((λ (mk-sum)
+    (mk-sum mk-sum))
+  (λ (mk-sum)
+    (λ (n m)
+      (if (zero? n)
+          m
+          (add1 ((mk-sum mk-sum) (sub1 n) m))))))
+ 2 3)
+
+;; mk = λt.t (mk t)
+;; mkmk = λk.λt.t ((k k) t)
+;; mk = (mkmk mkmk)
+
+(((λ (t)
+  ((λ (u) (u u))
+   (λ (k)
+     (t (lambda (v1 v2)
+          (((k k) t) v1 v2))))))
+ (λ (sum)
+    (λ (n m)
+      (if (zero? n)
+          m
+          (add1 (sum (sub1 n) m))))))
+ 2 3)
+  
+
+;; Y = λf.(λx.f (x x)) (λx.f (x x))
+
+(((lambda (f)
+  ((lambda (u) (u u))
+   (lambda (x)
+     (f (lambda (v1 v2) ((x x) v1 v2))))))
+ (λ (sum)
+    (λ (n m)
+      (if (zero? n)
+          m
+          (add1 (sum (sub1 n) m))))))
+ 2 3)
