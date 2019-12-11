560667113d9e007180bae2c5b883ff592887ae57
Mu Xian Ming <marsmxm@gmail.com>
2019-12-12 00:00:22 +0800

    update

--- redex/book.rkt
+++ redex/book.rkt
@@ -10,12 +10,22 @@
      (V B C)
      hole])
 
+(define B1 (term true))
+(define B2 (term false))
+(define B3 (term (V true false)))
+(define B4 (term (V ,B1 ,B2)))
+(define B5 (term (V false ,B4)))
+
 ;; Exercise 11.2
 (define-language add-lang
   [A 0
      1
      2
-     (+ A A)]
-  [C (+ C A)
-     (+ A C)
+     (+ A A A ...)]
+  [C (+ C A A ...)
+     (+ A A ... C A ...)
      hole])
+
+(redex-match add-lang
+             (in-hole C (+ 0 A))
+             (term (+ 0 (+ 0 1))))
