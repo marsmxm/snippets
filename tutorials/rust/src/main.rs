65e1b7ebaab0f24a494d64bedf1249f6a4a6e679
mu-xianming <mu.xianming@linecorp.com>
2020-05-14 18:02:50 +0800

    update

--- tutorials/rust/src/main.rs
+++ tutorials/rust/src/main.rs
@@ -1,3 +1,23 @@
+#[derive(Debug)]
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
 fn main() {
     println!("hello");
+
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    println!("{:?}", user1);
+
+    let b = Box::new(5);
+    println!("b = {}", b);
 }
