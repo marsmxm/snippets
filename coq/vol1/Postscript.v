fbc4fc01160b367d3de09ab4b6edaa83365d49f8
mu-xianming <mu.xianming@linecorp.com>
2021-06-03 09:59:00 +0800

    update software-foundations LF

--- coq/vol1/Postscript.v
+++ coq/vol1/Postscript.v
@@ -1,6 +1,7 @@
 (** * Postscript *)
 
-(** Congratulations: We've made it to the end! *)
+(** Congratulations: We've made it to the end of _Logical
+    Foundations_! *)
 
 (* ################################################################# *)
 (** * Looking Back *)
@@ -35,23 +36,16 @@
 (* ################################################################# *)
 (** * Looking Forward *)
 
-(** If what you've seen so far has whetted your interest, you have two
-    choices for further reading in the _Software Foundations_ series:
-
-           - _Programming Language Foundations_ (volume 2, by a set of
-             authors similar to this book's) covers material that
-             might be found in a graduate course on the theory of
-             programming languages, including Hoare logic, operational
-             semantics, and type systems.
-
-           - _Verified Functional Algorithms_ (volume 3, by Andrew
-             Appel) builds on the themes of functional programming and
-             program verification in Coq, addressing a range of topics
-             that might be found in a standard data structures course,
-             with an eye to formal verification. *)
+(** If what you've seen so far has whetted your interest, you have
+    several choices for further reading in later volumes of the
+    _Software Foundations_ series.  Some of these are intended to be
+    accessible to readers immediately after finishing _Logical
+    Foundations_; others require a few chapters from Volume 2,
+    _Programming Language Foundations_.  The Preface chapter in each
+    volume gives details about prerequisites. *)
 
 (* ################################################################# *)
-(** * Other sources *)
+(** * Resources *)
 
 (** Here are some other good places to learn more...
 
@@ -88,4 +82,4 @@
          https://deepspec.org/event/dsss17/index.html
 *)
 
-(* Wed Jan 9 12:02:47 EST 2019 *)
+(* 2021-05-26 09:56 *)
