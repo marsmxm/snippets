ab73876aff72237a957d0f2d76ee62af4c653380
Mu Xian Ming <marsmxm@gmail.com>
2021-12-01 20:45:56 +0800

    update

--- friedman/j-bob/scheme/ex.scm
+++ friedman/j-bob/scheme/ex.scm
@@ -261,3 +261,25 @@
      ((A) (if-same (atom y) 't))
      (() (if-same (ctx? x) 't))
      )))
+
+(J-Bob/prove (defun.atoms)
+  '(((defun my-list-induction (x)
+       (if (atom x)
+           '()
+           (my-list-induction (cdr x))))
+     (size x)
+     ((Q) (natp/size x))
+     (() (if-true
+          (if (atom x)
+              't
+              (< (size (cdr x)) (size x)))
+          'nil))
+     ((E) (size/cdr x))
+     (() (if-same (atom x) 't)))
+    
+    ((dethm set?/t (xs)
+       (if (set? xs) (equal (set? xs) 't) 't))
+     (list-induction xs))
+    ))
+
+    
