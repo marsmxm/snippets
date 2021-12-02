e00fb536ebe93c3443bbcdee303c31aeb667c50c
mu.xianming <marsmxm@gmail.com>
2021-12-02 10:16:51 +0800

    corr.R

--- r-prog/Assignment1/corr.R
+++ r-prog/Assignment1/corr.R
@@ -1,22 +1,24 @@
 library("stringr")
 library("readr")
-
+source("complete.R")
 
 corr <- function(directory, threshold = 0) {
-    cors <- rep(NA, 332)
-    for (monitor in 1:332) {
-        print(paste("Processing: ", monitor, sep = ""))
+    c_data <- complete("specdata")
+    ids <- c_data$id[c_data$nobs > threshold]
+    cors <- rep(NA, length(ids))
+    
+    index <- 0
+    for (monitor in ids) {
+        index <- index + 1
+        # print(paste("Processing: ", monitor, sep = ""))
         filename <- str_interp("${directory}/$[03d]{monitor}.csv")
         table <- read_csv(filename, col_types = "Dddi")
         s_data <- table[["sulfate"]]
         n_data <- table[["nitrate"]]
         sc <- s_data[!is.na(s_data) & !is.na(n_data)]
         nc <- n_data[!is.na(s_data) & !is.na(n_data)]
-        s_n_cor <- cor(sc, nc)
-        if (!is.na(s_n_cor) & s_n_cor > threshold) {
-            cors[[monitor]] <- s_n_cor
-        }
+        cors[[index]] <- cor(sc, nc)
     }
     
-    cors[!is.na(cors)]
+    cors
 }
