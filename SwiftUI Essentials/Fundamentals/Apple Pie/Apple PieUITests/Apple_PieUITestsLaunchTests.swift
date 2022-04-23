0841afcbad0f391ef1e7bbde8c7c1ebc9c075378
Mu Xian Ming <marsmxm@gmail.com>
2022-04-23 21:55:35 +0800

    update

---
+++ SwiftUI Essentials/Fundamentals/Apple Pie/Apple PieUITests/Apple_PieUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Apple_PieUITestsLaunchTests.swift
+//  Apple PieUITests
+//
+//  Created by Xianming Mu on 2022/4/20.
+//
+
+import XCTest
+
+class Apple_PieUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
---
+++ test-spark-parser/src/main/java/ink/mxm/Main.java
@@ -0,0 +1,14 @@
+package ink.mxm;
+
+
+import org.apache.spark.sql.catalyst.expressions.Expression;
+import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan;
+import org.apache.spark.sql.execution.SparkSqlParser;
+
+public class Main {
+    public static void main(String[] args) {
+        SparkSqlParser parser = new SparkSqlParser();
+        LogicalPlan plan = parser.parsePlan("SELECT a FROM t");
+        System.out.println(plan);
+    }
+}
