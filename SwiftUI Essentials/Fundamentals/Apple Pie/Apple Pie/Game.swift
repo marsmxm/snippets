fca4f56deb1cb544771df8d82ef5cad88ef99e25
mu.xianming <mu.xianming@ext-lmwn.com>
2022-05-07 17:28:19 +0800

    update

---
+++ SwiftUI Essentials/Fundamentals/Apple Pie/Apple Pie/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by CL10102-M on 2022/5/7.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
