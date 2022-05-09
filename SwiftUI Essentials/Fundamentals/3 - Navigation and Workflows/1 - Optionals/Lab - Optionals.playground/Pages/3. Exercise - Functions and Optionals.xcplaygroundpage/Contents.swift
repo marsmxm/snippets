1f3697deeb27f601e1374b5b975cc2ad23f0e45a
Sam <mu.xianming@ext-lmwn.com>
2022-05-09 17:11:30 +0800

    update

--- SwiftUI Essentials/Fundamentals/3 - Navigation and Workflows/1 - Optionals/Lab - Optionals.playground/Pages/3. Exercise - Functions and Optionals.xcplaygroundpage/Contents.swift
+++ SwiftUI Essentials/Fundamentals/3 - Navigation and Workflows/1 - Optionals/Lab - Optionals.playground/Pages/3. Exercise - Functions and Optionals.xcplaygroundpage/Contents.swift
@@ -5,7 +5,22 @@
  */
 let userInputAge: String = "34e"
 
+func checkAge(_ ageString: String) -> Int? {
+    if let age = Int(ageString) {
+        if age > 18 {
+            print("Welcome!")
+        } else {
+            print("Sorry, but you aren't old enough to use our app.")
+        }
+        return age
+    } else {
+        print("Sorry, something went wrong. Can you please re-enter your age?")
+        return nil
+    }
+}
 
+checkAge(userInputAge)
+checkAge("34")
 //:  Go back and update your function to return the age as an integer. Will your function always return a value? Make sure your return type accurately reflects this. Call the function and print the return value.
 
 
@@ -13,7 +28,13 @@ let userInputAge: String = "34e"
 var prices = ["Chips": 2.99, "Donuts": 1.89, "Juice": 3.99, "Apple": 0.50, "Banana": 0.25, "Broccoli": 0.99]
 var stock = ["Chips": 4, "Donuts": 0, "Juice": 12, "Apple": 6, "Banana": 6, "Broccoli": 3]
 
-
+func costOf(_ product: String) -> Double? {
+    if let count = stock[product], count > 0 {
+        return prices[product]!
+    } else {
+        return nil
+    }
+}
 /*:
 [Previous](@previous)  |  page 3 of 6  |  [Next: App Exercise - Food Functions](@next)
  */
---
+++ SwiftUI Essentials/Fundamentals/TrafficSegues/TrafficSegues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by CL10102-M on 2022/5/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
