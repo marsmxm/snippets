2db8e750a2fb25570dbd246d12c91b2b9138fd5d
Sam <mu.xianming@ext-lmwn.com>
2022-05-09 17:11:30 +0800

    update
    Former-commit-id: 373d53f6040bf2258d78a3974c905223c828b6dd

--- SwiftUI Essentials/Fundamentals/3 - Navigation and Workflows/1 - Optionals/Lab - Optionals.playground/Pages/5. Exercise - Failable Initializers.xcplaygroundpage/Contents.swift
+++ SwiftUI Essentials/Fundamentals/3 - Navigation and Workflows/1 - Optionals/Lab - Optionals.playground/Pages/5. Exercise - Failable Initializers.xcplaygroundpage/Contents.swift
@@ -3,9 +3,25 @@
 
  Create a `Computer` struct with two properties, `ram` and `yearManufactured`, where both parameters are of type `Int`. Create a failable initializer that will only create an instance of `Computer` if `ram` is greater than 0, and if `yearManufactured` is greater than 1970, and less than 2020.
  */
-
+struct Computer {
+    var ram: Int
+    var yearManufactured: Int
+    
+    init?(ram: Int, year: Int) {
+        if ram > 0 && year > 1970 && year < 2020 {
+            self.ram = ram
+            yearManufactured = year
+        } else {
+            return nil
+        }
+    }
+}
 
 //:  Create two instances of `Computer?` using the failable initializer. One instance should use values that will have a value within the optional, and the other should result in `nil`. Use if-let syntax to unwrap each of the `Computer?` objects and print the `ram` and `yearManufactured` if the optional contains a value.
+let c1 = Computer(ram: 0, year: 2000)
+let c2 = Computer(ram: 100, year: 2000)
+print(c1?.yearManufactured)
+print(c2?.ram)
 
 
 /*:
