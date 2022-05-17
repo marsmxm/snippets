84da2d9542d793deb610e2647fa50794ffa8627e
Sam <mu.xianming@ext-lmwn.com>
2022-05-17 08:28:51 +0800

    update

---
+++ SwiftUI Essentials/Fundamentals/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by CL10102-M on 2022/5/12.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
