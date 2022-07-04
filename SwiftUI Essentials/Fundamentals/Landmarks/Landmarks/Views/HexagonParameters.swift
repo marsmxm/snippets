98bed6356fab67f2bec735830b422ce5071ac844
Mu Xian Ming <marsmxm@gmail.com>
2022-07-04 23:00:24 +0800

    add ml v2

---
+++ SwiftUI Essentials/Fundamentals/Landmarks/Landmarks/Views/HexagonParameters.swift
@@ -0,0 +1,17 @@
+//
+//  HexagonParameters.swift
+//  Landmarks
+//
+//  Created by Xianming Mu on 2022/6/5.
+//
+
+import Foundation
+import CoreGraphics
+
+struct HexagonParameters {
+    struct Segment {
+        let line: CGPoint
+        let curve: CGPoint
+        let control: CGPoint
+    }
+}
