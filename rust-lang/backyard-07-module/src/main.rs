02956dd422ee01c912f575b7486612e60c9b12bc
Mu Xian Ming <marsmxm@gmail.com>
2023-12-14 23:58:22 +0800

    update

--- rust-lang/backyard-07-module/src/main.rs
+++ rust-lang/backyard-07-module/src/main.rs
@@ -1,5 +1,8 @@
-mod garden;
+use crate::garden::vegetables::Asparagus;
+
+pub mod garden;
 
 fn main() {
-    println!("Hello, world!");
+    let plant = Asparagus {};
+    println!("I'm growing {:?}!", plant);
 }
