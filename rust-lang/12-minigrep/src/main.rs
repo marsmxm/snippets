349b1ae6fcd5af27f3897658ed1c3499f839b5af
Mu Xian Ming <marsmxm@gmail.com>
2024-01-08 23:30:08 +0800

    rust 12

--- rust-lang/12-minigrep/src/main.rs
+++ rust-lang/12-minigrep/src/main.rs
@@ -1,33 +1,15 @@
-use std::{env, fs, process};
+use std::{env, process};
+use minigrep::{Config, run};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
-    let contents = fs::read_to_string(config.file_path).expect("should have been able to read the file");
-    println!("With text:\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
     }
 }
