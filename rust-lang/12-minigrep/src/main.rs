25d4256f948ae5fda6300ee2d72c1430fc8fcba1
Mu Xianming <mu.xianming@lmwn.com>
2024-01-12 15:15:16 +0800

    rust 13
    Former-commit-id: 57eaf6998dd252c4309454b2ddcd9365e0569ec3

--- rust-lang/12-minigrep/src/main.rs
+++ rust-lang/12-minigrep/src/main.rs
@@ -2,8 +2,7 @@ use std::{env, process};
 use minigrep::{Config, run};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
