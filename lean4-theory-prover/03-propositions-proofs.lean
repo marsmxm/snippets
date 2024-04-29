8f820567e372f625bc9cb1ca7d53f8528b29589b
Mu Xianming <mu.xianming@lmwn.com>
2024-04-29 16:58:01 +0800

    update

--- lean4-theory-prover/03-propositions-proofs.lean
+++ lean4-theory-prover/03-propositions-proofs.lean
@@ -1,9 +1,47 @@
-#check And     -- Prop → Prop → Prop
-#check Or      -- Prop → Prop → Prop
-#check Not     -- Prop → Prop
--- #check Implies
+variable (p q : Prop)
 
-variable (p q r : Prop)
-#check And p q                      -- Prop
-#check Or (And p q) r               -- Prop
-#check implies (And p q) (And q p)  -- Prop
+#check p → q → p ∧ q
+
+
+example (hp : p) (hq : q) : p ∧ q := And.intro hp hq
+#check fun (hp : p) (hq : q) => And.intro hp hq
+
+example (h : p ∧ q) : p := And.left h
+example (h : p ∧ q) : q := And.right h
+
+variable (hp : p) (hq : q)
+#check (⟨hp, hq⟩ : p ∧ q)
+
+example (hp : p) : p ∨ q := Or.intro_left q hp
+example (hq : q) : p ∨ q := Or.intro_right p hq
+
+
+example (h : p ∧ q) : q ∧ p :=
+  And.intro (And.right h) (And.left h)
+
+
+example (h : p ∨ q) : q ∨ p :=
+  Or.elim h
+    (fun hp : p =>
+      Or.intro_right q hp)
+    (fun hq : q =>
+      show q ∨ p from Or.intro_left p hq)
+
+example (hpq : p → q) (hnq : ¬q) : ¬p :=
+  fun hp : p => hnq (hpq hp)
+
+example (hp : p) (hnp : ¬p) : q := absurd hp hnp
+
+#check True.intro
+
+theorem and_swap : p ∧ q ↔ q ∧ p :=
+  Iff.intro
+    (fun h : p ∧ q =>
+     show q ∧ p from And.intro (And.right h) (And.left h))
+    (fun h : q ∧ p =>
+     show p ∧ q from And.intro (And.right h) (And.left h))
+
+#check and_swap p q    -- p ∧ q ↔ q ∧ p
+
+variable (h : p ∧ q)
+example : q ∧ p := Iff.mp (and_swap p q) h
