18497e8aa340d8c56525af13b56d5ff9e6015c25
Mu Xian Ming <marsmxm@gmail.com>
2024-05-01 07:49:27 +0800

    update

--- antlr-code/src/main/java/ink/mxm/tour/ExprJoyRide.java
+++ antlr-code/src/main/java/ink/mxm/tour/ExprJoyRide.java
@@ -8,19 +8,19 @@ package ink.mxm.tour; /***
 ***/
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
-import java.io.FileInputStream;
-import java.io.InputStream;
+
 public class ExprJoyRide {
     public static void main(String[] args) throws Exception {
         String inputFile = null; 
         if ( args.length>0 ) inputFile = args[0];
-        InputStream is = System.in;
-        if ( inputFile!=null ) is = new FileInputStream(inputFile);
-        ANTLRInputStream input = new ANTLRInputStream(is); 
-        ExprLexer lexer = new ExprLexer(input); 
-        CommonTokenStream tokens = new CommonTokenStream(lexer); 
-        ExprParser parser = new ExprParser(tokens); 
-        ParseTree tree = parser.prog(); // parse; start at prog <label id="code.tour.main.6"/>
-        System.out.println(tree.toStringTree(parser)); // print tree as text <label id="code.tour.main.7"/>
+
+        if (inputFile != null) {
+            CharStream input = CharStreams.fromFileName(inputFile);
+            ExprLexer lexer = new ExprLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            ExprParser parser = new ExprParser(tokens);
+            ParseTree tree = parser.prog(); // parse; start at prog <label id="code.tour.main.6"/>
+            System.out.println(tree.toStringTree(parser)); // print tree as text <label id="code.tour.main.7"/>
+        }
     }
 }
