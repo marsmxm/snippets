78c8b0d234a681979031ea4a76ef03d7f7bfaa73
Mu Xianming <mu.xianming@lmwn.com>
2024-06-24 17:29:24 +0800

    eopl ex 7.5

--- friedman/eopl3/chapter7/checked-ex/environments.scm
+++ friedman/eopl3/chapter7/checked-ex/environments.scm
@@ -33,9 +33,9 @@
 	  (if (eqv? search-sym bvar)
 	    bval
 	    (apply-env saved-env search-sym)))
-        (extend-env-rec (p-name b-var p-body saved-env)
+        (extend-env-rec (p-name b-vars p-body saved-env)
           (if (eqv? search-sym p-name)
-             (proc-val (procedure b-var p-body env))          
+             (proc-val (procedure b-vars p-body env))          
              (apply-env saved-env search-sym))))))
     
   )
