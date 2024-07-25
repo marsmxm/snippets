c924ae1b7507a4cbfff0a79fdb51bf315d17e2cd
Mu Xianming <mu.xianming@lmwn.com>
2024-07-25 17:30:43 +0800

    update

--- rust/rust-lang/lists/src/fifth.rs
+++ rust/rust-lang/lists/src/fifth.rs
@@ -1,8 +1,6 @@
-use std::mem;
-
-pub struct List<T> {
+pub struct List<'a, T> {
     head: Link<T>,
-    tail: Link<T>, // NEW!
+    tail: Option<&'a mut Node<T>>, // NEW!
 }
 
 type Link<T> = Option<Box<Node<T>>>;
@@ -12,7 +10,7 @@ struct Node<T> {
     next: Link<T>,
 }
 
-impl<T> List<T> {
+impl<'a, T> List<'a, T> {
     pub fn new() -> Self {
         List { head: None, tail: None }
     }
@@ -24,18 +22,64 @@ impl<T> List<T> {
             next: None,
         });
 
-        // swap the old tail to point to the new tail
-        let old_tail = mem::replace(&mut self.tail, Some(new_tail));
-
-        match old_tail {
-            Some(mut old_tail) => {
+        // Put the box in the right place, and then grab a reference to its Node
+        let new_tail = match self.tail.take() {
+            Some(old_tail) => {
                 // If the old tail existed, update it to point to the new tail
                 old_tail.next = Some(new_tail);
+                old_tail.next.as_deref_mut()
             }
             None => {
                 // Otherwise, update the head to point to it
                 self.head = Some(new_tail);
+                self.head.as_deref_mut()
             }
-        }
+        };
+
+        self.tail = new_tail;
+    }
+
+    pub fn pop(&'a mut self) -> Option<T> {
+        self.head.take().map(|old_head| {
+            self.head = old_head.next;
+            if self.head.is_none() {
+                self.tail = None;
+            }
+            old_head.elem
+        })
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::List;
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
     }
 }
