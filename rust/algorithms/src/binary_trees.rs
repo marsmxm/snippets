6b0e0d01d30d9ed1a549acc6bc928b7c6c4d6254
Mu Xianming <mu.xianming@lmwn.com>
2024-08-07 17:06:40 +0800

    update

--- rust/algorithms/src/binary_trees.rs
+++ rust/algorithms/src/binary_trees.rs
@@ -8,6 +8,7 @@ pub struct BinaryTree<T> {
 
 type Link<T> = Option<NonNull<Node<T>>>;
 
+#[derive(Debug)]
 pub struct Node<T> {
     key: T,
     parent: Link<T>,
@@ -23,6 +24,11 @@ impl<T: Display> Node<T> {
             left: None,
             right: None,
         }
+    }
+
+    unsafe fn new_ptr(node: Node<T>) -> NonNull<Node<T>> {
+        NonNull::new_unchecked(
+                Box::into_raw(Box::new(node)))
     }
 
     fn to_ascii_tree(&self) -> Tree {
@@ -54,112 +60,199 @@ impl<T: Display + PartialOrd> BinaryTree<T> {
     pub fn new(root: Node<T>) -> Self {
         unsafe {
             BinaryTree {
-                root: Some(
-                    NonNull::new_unchecked(Box::into_raw(Box::new(root)))
-                )
+                root: Some(Node::new_ptr(root))
             }
         }
     }
 
     pub fn print(&self) {
         let none_node = Tree::Leaf(vec![String::from("none")]);
-        let ascii_tree = self.root.as_ref().map_or(none_node, |node| node.to_ascii_tree());
-        let mut output = String::new();
-        write_tree(&mut output, &ascii_tree);
-        println!("{}", output);
+        unsafe {
+            let ascii_tree = self.root.as_ref().map_or(none_node, |node| (*node.as_ptr()).to_ascii_tree());
+            let mut output = String::new();
+            write_tree(&mut output, &ascii_tree);
+            println!("{}", output);
+        }
+        
     }
 
     pub fn insert(&mut self, k: T) {
-        let mut new_node = Box::new(Node::new(k));
+        unsafe {
+            let mut new_node = Node::new(k);
 
-        let mut cursor: Option<&mut Box<Node<T>>> = self.root.as_mut();
-        let mut parent: Option<&mut Box<Node<T>>> = None;
+            let mut cursor = self.root;
+            let mut parent = None;
 
-        while let Some(cursor_node) = cursor {
-            parent = cursor;
+            while let Some(cursor_ptr) = cursor {
+                parent = cursor;
 
-            if new_node.key < cursor_node.key {
-                cursor = cursor_node.left.as_mut();
+                if new_node.key < (*cursor_ptr.as_ptr()).key {
+                    cursor = (*cursor_ptr.as_ptr()).left;
+                } else {
+                    cursor = (*cursor_ptr.as_ptr()).right;
+                }
+            }
+
+            if let Some(parent_ptr) = parent {
+                new_node.parent = parent;
+
+                if new_node.key < (*parent_ptr.as_ptr()).key {
+                    (*parent_ptr.as_ptr()).left = Some(Node::new_ptr(new_node));
+                } else {
+                    (*parent_ptr.as_ptr()).right = Some(Node::new_ptr(new_node));
+                }
             } else {
-                cursor = cursor_node.right.as_mut();
+                self.root  = Some(Node::new_ptr(new_node));
             }
         }
-
-        if let Some(parent_node) = parent {
-            new_node.parent = Some(&*parent_node);
-            if new_node.key < parent_node.key {
-                parent_node.left = Some(new_node);
-            } else {
-                parent_node.right = Some(new_node);
-            }
-        } else {
-            self.root  = Some(new_node);
-        }
-    
     }
 
     pub fn walk(&self) {
-        fn link_walk<T: Display + PartialOrd>(link: &Link<T>) {
-            if let Some(node) = link.as_ref() {
-                link_walk(&node.left);
-                println!("{}", node.key);
-                link_walk(&node.right);
+        fn link_walk<T: Display + PartialOrd>(link: Link<T>) {
+            if let Some(node) = link {
+                unsafe {
+                    link_walk((*node.as_ptr()).left);
+                    println!("{}", (*node.as_ptr()).key);
+                    link_walk((*node.as_ptr()).right);
+                }
+                
             }
         }
 
-        link_walk(&self.root);
+        link_walk(self.root);
     }
 
-    // pub fn search(&self, k: T) -> Option<Ref<Node<T>>> {
-    //     fn search_link<T: Display + PartialOrd>(link: Option<Ref<Node<T>>>, k: T) -> Option<Ref<Node<T>>> {
-    //         if let Some(node) = link.as_ref() {
-    //             if k == node.borrow().key {
-    //                 return Some(node.borrow());
-    //             } else if k < node.borrow().key {
-    //                 return search_link(node.borrow().map(|link| link.left, k);
-    //             } else {
-    //                 return search_link(node.borrow().right, k);
-    //             }
-    //         } else {
-    //             return None;
-    //         }
-    //     }
+    pub fn search(&self, k: T) -> Option<&Node<T>> {
+        fn search_link<'a, T: Display + PartialOrd>(link: Link<T>, k: T) -> Option<&'a Node<T>> {
+            if let Some(node) = link {
+                unsafe {
+                    if k == (*node.as_ptr()).key {
+                        return Some(node.as_ref());
+                    } else if k < (*node.as_ptr()).key {
+                        return search_link((*node.as_ptr()).left, k);
+                    } else {
+                        return search_link((*node.as_ptr()).right, k);
+                    }
+                }
+            } else {
+                return None;
+            }
+        }
 
-    //     search_link(self.root.map(|link| link.borrow(), k)
-    // }
+        search_link(self.root, k)
+    }
 
-    // pub fn search_iter(&self, k: T) -> Option<Ref<Node<T>>> {
-    //     let mut cursor = &self.root;
-    //     while let Some(node) = cursor.as_ref() {
-    //         if k == node.borrow().key {
-    //             return Some(node.borrow());
-    //         } else if k < node.borrow().key {
-    //             cursor = &node.borrow().left;
-    //         } else {
-    //             cursor = &node.borrow().right;
-    //         }
-    //     }
+    pub fn search_iter(&self, k: T) -> Option<&Node<T>> {
+        let mut cursor = self.root;
+        while let Some(node) = cursor {
+            unsafe {
+                if k == (*node.as_ptr()).key {
+                    return Some(node.as_ref());
+                } else if k < (*node.as_ptr()).key {
+                    cursor = (*node.as_ptr()).left;
+                } else {
+                    cursor = (*node.as_ptr()).right;
+                }
+            }
+        }
 
-    //     return None;
-    // }
+        return None;
+    }
 
-    // pub fn minimum(&self) -> Option<Ref<Node<T>>> {
-    //     let mut cursor = &self.root;
+    pub fn minimum(&self) -> Option<&Node<T>> {
+        unsafe {
+            let mut cursor = self.root;
+            let mut parent = None;
 
-    //     while let Some(node) = cursor.as_ref() {
-    //         cursor = &node.borrow().left;
-    //     }
+            while let Some(node) = cursor {
+                parent = cursor;
+                cursor = (*node.as_ptr()).left;
+            }
 
-    //     cursor.as_ref().map(|node| node.borrow())
-    // }
+            return parent.map(|node| node.as_ref());
+        }
+    }
 
-    // pub fn maximum(&self) -> Option<Ref<Node<T>>>  {
-    //     let mut cursor = &self.root;
+    pub fn maximum(&self) -> Option<&Node<T>>  {
+        unsafe {
+            let mut cursor = self.root;
+            let mut parent = None;
 
-    //     while let Some(node) = cursor.as_ref() {
-    //         cursor = &node.borrow().right;
-    //     }
+            while let Some(node) = cursor {
+                parent = cursor;
+                cursor = (*node.as_ptr()).right;
+            }
 
-    //     cursor.as_ref().map(|node| node.borrow())
-    // }
+            return parent.map(|node| node.as_ref());
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::BinaryTree;
+
+    #[test]
+    fn test_print_tree() {
+        let mut tree = BinaryTree::empty();
+
+        tree.insert(2);
+        tree.insert(6);
+        tree.insert(4);
+        tree.insert(1);
+
+        tree.print();
+    }
+
+    #[test]
+    fn test_search() {
+        let mut tree = BinaryTree::empty();
+
+        tree.insert(2);
+        tree.insert(6);
+        tree.insert(4);
+        tree.insert(1);
+
+        assert_eq!(6, tree.search(6).unwrap().key);
+        assert!(tree.search(16).is_none());
+    }
+
+    #[test]
+    fn test_search_iter() {
+        let mut tree = BinaryTree::empty();
+
+        tree.insert(2);
+        tree.insert(6);
+        tree.insert(4);
+        tree.insert(1);
+
+        assert_eq!(6, tree.search_iter(6).unwrap().key);
+        assert!(tree.search_iter(16).is_none());
+    }
+
+    #[test]
+    fn test_minimum() {
+        let mut tree = BinaryTree::empty();
+
+        tree.insert(2);
+        tree.insert(6);
+        tree.insert(4);
+        tree.insert(1);
+
+        let op = tree.minimum();
+        assert_eq!(1, op.unwrap().key);
+    }
+
+    #[test]
+    fn test_maximum() {
+        let mut tree = BinaryTree::empty();
+
+        tree.insert(2);
+        tree.insert(6);
+        tree.insert(4);
+        tree.insert(1);
+
+        let op = tree.maximum();
+        assert_eq!(6, op.unwrap().key);
+    }
 }
